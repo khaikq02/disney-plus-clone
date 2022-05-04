c44f4e11f9d71cd2f22c2f8a0b11b60a2e37e759
Wiliam Kieu <khaikqdev@gmail.com>
2022-05-04 15:07:52 +0700

    done slider bar with react slick

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import ImgSlider from "./ImgSlider"
+import ImgSlider from "./ImgSlider";
 function Home() {
-  return <Container>
-    
-    <ImgSlider/>
-  </Container>;
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
 }
 
 export default Home;
@@ -14,6 +15,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow: hidden;
 
   &:before {
     content: "";
